481307590dc7c7d359ddbd9d81b689a52ba2e37b
fixitalredy <130216446+fixitalredy@users.noreply.github.com>
2023-04-25 13:57:00 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let readMore = document.querySelector(".read-more");
+let brandsList = document.querySelector(".brands__list")
+readMore.addEventListener('click',function(){
+brandsList.style.height = `${brandsList.scrollHeight}px`;
+})
