1ac33970baf194e91b4ce4229a9bd21b6568fd56
fixitalredy <130216446+fixitalredy@users.noreply.github.com>
2023-04-25 15:55:52 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 let readMore = document.querySelector(".read-more");
 let brandsList = document.querySelector(".brands__list")
-readMore.addEventListener('click',function(){
-brandsList.style.height = `${brandsList.scrollHeight}px`;
+
+readMore.addEventListener("click", function(){
+  showMore()
 })
- 
+function showMore(){
+ if(readMore.textContent === "Показать все") {
+  brandsList.style.height = `${brandsList.scrollHeight}px`;
+  readMore.textContent = "Скрыть"
+}
+ else {
+ readMore.textContent = "Показать все"
+ brandsList.style.height = 160 + "px";
+ }
+}
