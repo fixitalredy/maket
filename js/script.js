7447fd25a4b3648d4c3bd2b5cabc209c9d6a3a2e
fixitalredy <130216446+fixitalredy@users.noreply.github.com>
2023-04-27 18:46:55 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
-
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
   direction: 'horizontal',
   loop: false,
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
 });
+let readMore = document.querySelector(".read-more");
+let brandsMain = document.querySelector(".brands__swiper-wrapper");
+
+readMore.addEventListener("click", function(){
+  showMore()
+});
+
+function showMore(){
+ if(readMore.textContent === "Показать все") {
+  brandsMain.style.height = `${brandsMain.scrollHeight}px`;
+  readMore.textContent = "Скрыть";
+  readMore.classList.toggle('read-more--arrow-up');
+}
+ else {
+ readMore.textContent = "Показать все"
+ brandsMain.style.height = 160 + "px";
+ readMore.classList.toggle('read-more--arrow-up');
+ }
+}
+
 
